6696b93e0d885591f6b53bb46c4a28e4318ef52f
Hostek <michalpl245674477@gmail.com>
2022-08-28 14:37:56 +0200

    Can read file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+import fs from "fs"
 import inquirer from "inquirer"
+import path from "path"
 import { supported_langs } from "./types"
 
 const lang_choices: supported_langs[] = ["cpp", "typescript"]
@@ -28,3 +30,18 @@ const answers = await inquirer.prompt([
 const env_dir: string = answers.env_dir
 const type: typeof lang_choices[number] = answers.type
 const create_example = answers.example === "yes"
+
+let file = fs.readFileSync(env_dir, { encoding: "utf-8" })
+
+file = file.replaceAll("\r", "")
+
+const each_line = file.split("\n")
+
+const values = each_line.map((line) => {
+    const [key, value] = line.split("=", 1)
+    return { key, value }
+})
+
+if (type === "cpp") {
+} else if (type === "typescript") {
+}
